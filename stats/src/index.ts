29505a143b6ce84e51bdcc2c12149916d3495787
Ha Kieu Minh <minhha19122014@gmail.com>
2019-10-27 13:17:02 +0900

    removed unnecessary code

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,8 @@
 import { MathReader } from './MatchReader';
-import { WinAnalysis } from './analyzers/WinAnalysis';
-// import { ConsoleRepoter } from './reporters/ConsoleReport';
 import { Summary } from './Summary';
-import { HtmlReport } from './reporters/HtmlReport';
 
 const mathReader = MathReader.fromCsvFile('football.csv');
 mathReader.load();
 
-// const winAnalysisData = new WinAnalysis('Man United');
-// const htmlReport = new HtmlReport();
-// const summary = new Summary(winAnalysisData, htmlReport);
 const summary = Summary.fromWinAnalysisAndHtmlReport('Man United');
 summary.print(mathReader.data);
