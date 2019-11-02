e77055f66d3c772cdaf38d70fe6fd0dfe1ca9e28
Ha Kieu Minh <minhha19122014@gmail.com>
2019-11-02 19:33:46 +0900

    init trigger and register events functions

--- MyFrameWork/models/User.ts
+++ MyFrameWork/models/User.ts
@@ -3,13 +3,33 @@ export interface UserProps {
     age?: number
 }
 
+export type CallBack = () => void;
+
 export class User {
     constructor(private data: UserProps){}
+    
+    events: {[key: string]: CallBack[]} = {};
 
     get(prop: string): number | string{
         return this.data[prop]
     }
+
     set(user: UserProps){
         Object.assign(this.data, user);
     }
+    
+    on(eventName: string | number, callback : CallBack){
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
+
+    trigger(eventName: string) : void{
+        const handlers = this.events[eventName];
+        if (!handlers || handlers.length === 0) {
+            return;
+        }
+       handlers.forEach(handler => handler());
+    }
+
 }
--- MyFrameWork/src/index.ts
+++ MyFrameWork/src/index.ts
@@ -2,4 +2,14 @@ import {User} from '../models/User';
 
 let newUser = new User({name: 'Ha', age: 19});
 newUser.set({age: 28});
-console.log(`Welcome to my project ${newUser.get('name')} with ${newUser.get('age')} age`);
+newUser.on('onClick', () => {
+    console.log('User clicked on item');
+});
+newUser.on('onClick', () => {});
+newUser.on('onHover', () => {
+    console.log('User hover in the item');
+});
+newUser.trigger('onClick');
+newUser.trigger('onHover');
+// console.log(newUser);
+// console.log(`Welcome to my project ${newUser.get('name')} with ${newUser.get('age')} age`);
