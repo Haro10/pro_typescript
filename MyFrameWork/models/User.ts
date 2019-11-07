62f8f5b220be33a7ea9a039da2f183768b0564e8
Ha Kieu Minh <minhha19122014@gmail.com>
2019-11-07 23:10:04 +0900

    added  json-server, fecth and save function

--- MyFrameWork/models/User.ts
+++ MyFrameWork/models/User.ts
@@ -1,4 +1,7 @@
+import axios, {AxiosResponse} from 'axios';
+
 export interface UserProps {
+    id?:number;
     name?: string;
     age?: number
 }
@@ -32,4 +35,19 @@ export class User {
        handlers.forEach(handler => handler());
     }
 
+    fetch(): void{
+        axios.get(`http://localhost:3000/users/${this.get('id')}`)
+        .then((response: AxiosResponse): void => {
+           return this.set(response.data);
+        })
+    }
+
+    save():void {
+        const id = this.get('id');
+        if (id) {
+            axios.put(`http://localhost:3000/users/${id}`, this.data);
+        } else {
+            axios.post(`http://localhost:3000/users`, this.data);
+        }
+    }
 }
--- MyFrameWork/src/index.ts
+++ MyFrameWork/src/index.ts
@@ -1,15 +1,17 @@
 import {User} from '../models/User';
 
-let newUser = new User({name: 'Ha', age: 19});
-newUser.set({age: 28});
-newUser.on('onClick', () => {
-    console.log('User clicked on item');
-});
-newUser.on('onClick', () => {});
-newUser.on('onHover', () => {
-    console.log('User hover in the item');
-});
-newUser.trigger('onClick');
-newUser.trigger('onHover');
-// console.log(newUser);
+let user1 = new User({id: 1 , name: 'Ha', age: 1900});
+let user2 = new User({name: 'Ha', age: 1900});
+// newUser.set({age: 28});
+// newUser.on('onClick', () => {
+//     console.log('User clicked on item', newUser.get('age'));
+// });
+// newUser.on('onClick', () => {});
+// newUser.on('onHover', () => {
+//     console.log('User hover in the item');
+// });
+// newUser.trigger('onClick');
+// newUser.trigger('onHover');
+user1.save();
+user2.save();
 // console.log(`Welcome to my project ${newUser.get('name')} with ${newUser.get('age')} age`);
