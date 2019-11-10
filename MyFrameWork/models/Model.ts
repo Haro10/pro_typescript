454482ee499a1ae69bf203f2e8bb394946b96647
Ha Kieu Minh <minhha19122014@gmail.com>
2019-11-10 12:54:44 +0900

    detached model to become reuseable, and make more flexible for adding new methods depend on your bussiness

---
+++ MyFrameWork/models/Model.ts
@@ -0,0 +1,73 @@
+import { AxiosPromise, AxiosResponse } from "axios";
+
+interface Attributes<T>{
+    get<K extends keyof T>(key:K):T[K],
+    set(update: T):void,
+    getAll(): T,
+}
+
+interface Eventing{
+    on(eventName: string, callBack: () => void): void,
+    trigger(eventName:string):void,
+}
+
+interface Sync<T>{
+    fetch(id:number):AxiosPromise,
+    save(data: T): AxiosPromise,
+}
+
+interface HasId{
+    id?: number
+}
+
+export class Model<T extends HasId> {
+    constructor(
+        private attributes: Attributes<T>,
+        private events: Eventing,
+        private sync: Sync<T>,
+        ){}
+
+        get get(){
+            return this.attributes.get;
+         }
+     
+        get set(){
+             return this.attributes.set;
+         }
+     
+        get on(){
+             return this.events.on;
+         }
+     
+        get trigger(){
+             return this.events.trigger;
+         }
+         
+        fetch(): void{
+             const id = this.get('id');
+             console.log(id);
+             if (typeof id !== 'number') {
+                 throw new Error('can not fetch without an Id');
+             }
+     
+             this.sync.fetch(id)
+             .then((response: AxiosResponse): void => {
+                 this.set(response.data);
+                 console.log(response.data);
+                 this.trigger('fetch');
+             })
+             .catch((err: any) => {
+                 throw(err);
+             })
+         }
+          
+        save(): void{
+             this.sync.save(this.attributes.getAll())
+             .then((response: AxiosResponse): void => {
+                 this.trigger('save');
+             })
+             .catch(() => {
+                 this.trigger('error');
+             })
+         }
+}
--- MyFrameWork/src/index.ts
+++ MyFrameWork/src/index.ts
@@ -1,14 +1,12 @@
 import {User} from '../models/User';
 
-let user1 = new User({id: 1 , name: 'Ha', age: 19});
+let user1 = User.buildUser({id: 1 , name: 'Ha', age: 19});
 user1.on('save', () => {
     console.log('we save data success ');
 })
 user1.on('fetch', () => {
     console.log('we fetch data success, and we will update other fields');
 })
-// user1.trigger('save');
-user1.fetch();
-user1.set({age: 28});
-user1.save();
-console.log(user1);
+
+user1.fetch()
+console.log(user1.get('name'));
