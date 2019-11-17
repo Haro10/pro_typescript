2e0550ca3cd4be8f5f5ae9e0f871b55a8d8b8e7a
Ha Kieu Minh <minhha19122014@gmail.com>
2019-11-17 13:32:58 +0900

    Bind events to element

--- MyFrameWork/src/views/UserForm.ts
+++ MyFrameWork/src/views/UserForm.ts
@@ -6,13 +6,40 @@ export class UserForm{
         <div>
             <h1>User Form</h1>
             <input></input>
+            <button>Click Me</button>
         </div>
         `
+    }
+
+    eventsMap(): {[key:string]: ()=> void} {
+        return {
+            'click:button': this.onButtonClick,
+            'mouseenter:button': this.onButtonHover,
+        };
+    }
+
+    onButtonClick(): void{
+        console.log('You clicked button');
+    }
+
+    onButtonHover(): void{
+        console.log('You hovered button');
+    }
+
+    bindEvents(fragment: DocumentFragment):void{
+        const eventsMap = this.eventsMap();
+        for (let eventKey in eventsMap){ // use let to get eventKey (key) in eventMaps(object)
+           const [eventName, selector]  = eventKey.split(':');
+           fragment.querySelectorAll(selector).forEach(element => {
+               element.addEventListener(eventName, eventsMap[eventKey])
+           })
+        }
     }
 
     render(): void{
         const templateElement = document.createElement('template'); // it means type template
         templateElement.innerHTML = this.template();
+        this.bindEvents(templateElement.content);
         this.parent.append(templateElement.content)
     }
 }
