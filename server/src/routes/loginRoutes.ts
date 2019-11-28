506fe8b1cb190c072f0dcbe0e2adf46ed331a7c6
Ha Kieu Minh <minhha19122014@gmail.com>
2019-11-28 23:33:26 +0900

    middleware bodyParser to take the email and password -> crear a body and  append them to body

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,9 +1,31 @@
-import {Router} from 'express';
+import {Router, Request, Response} from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
     res.send('hi there!');
+})
+
+router.get('/login', (req: Request, res: Response) => {
+    res.send(`
+        <form METHOD="POST">
+        <div>
+            <label>Email :</label>
+            <input name='email'></input>
+        </div>
+        <div>
+            <label>Password :</label>
+            <input name='password' type='password'></input>
+        </div>
+        <button>submit</button>
+        </form>
+    `);
+})
+
+router.post('/login', (res: Request, req: Response) => {
+  const { email, password } = res.body;
+
+   req.send('You are logged');
 })
 
 export { router };
