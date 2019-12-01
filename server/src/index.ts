c61638dd5c6b82bd8db8404d03a5bb34d45bbe31
Ha Kieu Minh <minhha19122014@gmail.com>
2019-12-01 10:45:49 +0900

    Don't write some stupid code like just because you want to use typescript class

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,32 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.urlencoded({ extended:true }));
-app.use(cookieSession({keys:['anything']}));
-app.use(router);
+// app.use(bodyParser.urlencoded({ extended:true }));
+// app.use(cookieSession({keys:['anything']}));
+// app.use(router);
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
-})
+// app.listen(3000, () => {
+//     console.log('listening on port 3000');
+// })
+
+//Don't write some stupid code like just because you want to use typescript class
+
+class Server{
+    app: express.Express = express();
+    
+    constructor(){
+        this.app.use(bodyParser.urlencoded({ extended:true }));
+        this.app.use(cookieSession({keys:['anything']}));
+        this.app.use(router);
+    }
+
+    start(){
+        this.app.listen(3000, () => {
+            console.log('listening on port 3000');
+        })
+    }
+}
+
+new Server().start();
